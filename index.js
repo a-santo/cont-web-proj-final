41343be7cf338929459128302ad5a68a05c0d743
André Santo <andre.miguel.santo@gmail.com>
2021-06-15 22:15:33 +0800

    Exemplo de autocarro em tempo real

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 let port = 3000;
@@ -9,8 +10,8 @@ app.get('/', function(req, res){
     res.render('index');
 });
 
-app.get('/teste', function(req, res){
-    res.render('teste');
+app.get('/autocarro/:id', function(req, res){
+    res.render('autocarro', { id: req.params.id, token: process.env.MAPBOX_TOKEN });
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
